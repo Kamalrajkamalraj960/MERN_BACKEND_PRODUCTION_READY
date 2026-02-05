d731d18177dd2cf1f4024b0fa71d5a780ec83501
= <kamalrajmanu@gmail.com>
2026-02-05 13:13:31 +0530

    DAY 3: Added validation middleware for register & login

--- src/server.js
+++ src/server.js
@@ -1,18 +1,7 @@
-import express from "express";
-import dotenv from "dotenv";
-import connectDB from "./config/db.js";
-import authRoutes from "./routes/authRoutes.js";
-import taskRoutes from "./routes/taskRoutes.js";
+import app from "./app.js";
 
-dotenv.config();
-connectDB();
+const PORT = 5000;
 
-const app = express();
-app.use(express.json());
-
-app.use("/api/auth", authRoutes);
-app.use("/api", taskRoutes);
-
-app.listen(process.env.PORT, () =>
-  console.log(`Server running on port ${process.env.PORT}`)
-);
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
+});
