8c585591686b8dc4a1ba169eefd60f5f7bf3118b
= <kamalrajmanu@gmail.com>
2026-02-11 18:44:38 +0530

    Add friendly homepage with links to APIs

--- src/app.js
+++ src/app.js
@@ -9,6 +9,19 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 
+// ✅ Cool root route with links
+app.get("/", (req, res) => {
+  res.send(`
+    <h1 style="font-family: sans-serif; color: #4A90E2;">🚀 MERN Backend is Live!</h1>
+    <p>Welcome! Here are your endpoints:</p>
+    <ul>
+      <li><a href="/api-docs" target="_blank">Swagger API Docs</a></li>
+      <li><a href="/api/users" target="_blank">GET All Users</a></li>
+    </ul>
+    <p>Use Thunder Client or Postman to test POST requests!</p>
+  `);
+});
+
 app.use("/api/users", userRoutes);
 
 // Swagger Config
